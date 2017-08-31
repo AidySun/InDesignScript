1d9a8759a080b427b25ed8dcee5cbe5b378ec5e0
AidySun <aidyszh@gmail.com>
2017-08-31 16:19:12 +0000

    add js files

---
+++ mergeToActiveLayer.js
@@ -0,0 +1,6 @@
+﻿
+var doc = app.activeDocument;
+var layer= doc.activeLayer;
+var xpressoLayer = doc.layers.itemByName("xPresso System Layer");
+
+layer.merge(xpressoLayer);
---
+++ remove_selected_links.js
@@ -0,0 +1,10 @@
+﻿if (app.documents.length == 0) {
+	alert("No documents are  open. Please open a document and try again.");
+} else {
+	var doc = app.activeDocument;
+	var links = doc.links;
+    
+    var len = app.selection.length;
+    alert(len);
+
+}
